c9fb0065309f808e48f6a4e0493bc8e4a450c01a
Harry Craven <HarryCraven.Dev@gmail.com>
2020-07-27 13:09:15 +0100

    Added new methods getItemInput, clearInput, addItemToList.

--- src/JS/Ui.js
+++ src/JS/Ui.js
@@ -15,4 +15,32 @@ export class UI {
 
     document.getElementById("item-list").innerHTML = html;
   };
+
+  getItemInput = () => {
+    return {
+      name: document.getElementById("item-name").value,
+      calories: document.getElementById("item-calories").value,
+    };
+  };
+
+  clearInput = () => {
+    document.getElementById("item-name").value = "";
+    document.getElementById("item-calories").value = "";
+  };
+
+  addItemToList = (item) => {
+    // Create 'li' for new Item
+    const li = document.createElement("li");
+    li.className = "collection-item";
+    li.id = `${item.id}`;
+    li.innerHTML = `
+    <strong>${item.name} : </strong><em>${item.calories} Calories</em>
+    <a href="#" class="secondary-content">
+        <i class="edit-item fa fa-pencil"></i>
+    </a>
+    `;
+
+    // Insert into DOM
+    document.getElementById("item-list").insertAdjacentElement("beforeend", li);
+  };
 }
